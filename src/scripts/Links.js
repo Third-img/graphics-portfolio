b7c872b704b37b2a4f10e866ef5d1294011735e1
Third-img <pascualquerubin06@gmail.com>
2026-04-04 15:04:32 +0800

    updated mobile version

---
+++ src/scripts/Links.js
@@ -0,0 +1,11 @@
+export const scrollToSection = (id) => {
+  const section = document.getElementById(id);
+
+  if (section) {
+    section.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+};
+
+export const instagram = () => {};
